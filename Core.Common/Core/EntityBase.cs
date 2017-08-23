af396eff2f6ff875f6ce6281d32626a7facdf634
SonDo <tsonmiramar@gmail.com>
2017-08-23 15:53:53 -0700

    Starter Code

---
+++ Core.Common/Core/EntityBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace Core.Common
+{
+    public class EntityBase : IExtensibleDataObject
+    {
+        #region IExtensibleDataObject Members
+        public ExtensionDataObject ExtensionData { get; set; }
+        #endregion
+    }
+}
